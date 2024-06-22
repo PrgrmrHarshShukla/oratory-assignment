7d0b4993d6f4e1a135b7e6f18159b3a59734a015
_ HARSH _ <ashuklaharsh2005@gmail.com>
2024-06-22 07:33:24 +0530

    Head1

--- src/components/head1.tsx
+++ src/components/head1.tsx
@@ -1,7 +1,6 @@
-import { FaRegBell } from "react-icons/fa";
+import { FaChevronDown, FaCommentAlt, FaRegBell, FaSuitcase } from "react-icons/fa";
 import logo from "../assets/images/logo.png"
-
-
+import { FaHandHoldingDollar } from "react-icons/fa6";
 const Head1 = () => {
     return (
         <div className="flex flex-row justify-between items-center w-full h-[8vh] px-8 border-b border-gray-200">
@@ -9,18 +8,34 @@ const Head1 = () => {
                 <span className="text-[#e76d44] font-bold">Logo</span>
             </div>
 
-            <div className="rounded-full w-1/4 border border-gray-200 flex flex-row justify-around items-center h-[6vh]">
-                <div>Jobs</div>
-                <div>Messages</div>
-                <div>Payments</div>
+            <div className="rounded-full w-1/4 border border-gray-200 flex flex-row justify-between px-2 items-center h-[6vh]">
+                <div className="flex flex-row justify-center items-center gap-2 border px-2 h-[4.5vh] cursor-pointer rounded-full bg-[#e5604f] hover:text-white hover:opacity-100">
+                    <FaSuitcase />
+                    <span className="opacity-55 font-semibold text-sm hover:text-white hover:opacity-100">Jobs</span>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-2 border px-2 h-[4.5vh] cursor-pointer rounded-full hover:bg-[#e5604f] hover:text-white hover:opacity-100">
+                    <div className="relative">
+                        <FaCommentAlt />
+                        <div className="w-[4px] h-[4px] rounded-full bg-[#e76d44] absolute -top-[2px] -right-[2px]"></div>
+                    </div>
+                    <span className="opacity-55 font-semibold text-sm hover:text-white hover:opacity-100">Messages</span>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-2 border px-2 h-[4.5vh] cursor-pointer rounded-full hover:bg-[#e5604f] hover:text-white hover:opacity-100">
+                    <FaHandHoldingDollar />
+                    <span className="opacity-55 font-semibold text-sm hover:text-white hover:opacity-100">Payments</span>
+                </div>
             </div>
 
-            <div className="flex flex-row justify-center items-center w-auto gap-2">
+            <div className="flex flex-row justify-center items-center w-auto gap-4">
                 <div className="relative">
                     <FaRegBell />
                     <div className="w-[4px] h-[4px] rounded-full bg-[#e76d44] absolute top-0 right-0"></div>
                 </div>
-                <img src={logo} alt="Logo" className="w-6 h-6 rounded-full" />
+                <div className="flex flex-row justify-center items-center gap-1">
+                    <img src={logo} alt="Logo" className="w-6 h-6 rounded-full" />
+                    <FaChevronDown color="" className="font-normal" />
+                </div>
+                
 
             </div>
 
